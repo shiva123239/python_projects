062b85884f4b769bc176ed70d9fb2c9fb4150796
shiva rama krishna <srkshiva48@gmail.com>
2025-08-31 17:45:00 +0530

    Add files via upload

---
+++ mini_project_primenum.py
@@ -0,0 +1,15 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if n % i == 0:
+            return False
+        return True
+
+num = int(input("enter a number:"))
+if is_prime(num):
+    print("its a prime number.")
+
+else:
+    print("its not a prime number.")
+
